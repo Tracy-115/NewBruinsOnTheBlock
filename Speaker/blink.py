d69f053f609de17a41cf0c65443e6d7295e2d6f9
Tracy-115 <kmin984@insite.4cd.edu>
2026-08-07 17:20:12 -0700

    multiple buttons can be pushed at the same time

--- Speaker/blink.py
+++ Speaker/blink.py
@@ -3,6 +3,7 @@ import math
 import struct
 
 sample_rate = 8000
+chunk_size = 64
 
 audio = I2S(
     0,
@@ -13,7 +14,7 @@ audio = I2S(
     bits=16,
     format=I2S.MONO,
     rate=sample_rate,
-    ibuf=4000
+    ibuf=8000
 )
 
 potent = ADC(26)
@@ -26,44 +27,104 @@ button5 = Pin(7, Pin.IN, Pin.PULL_UP)
 button6 = Pin(8, Pin.IN, Pin.PULL_UP)
 button7 = Pin(10, Pin.IN, Pin.PULL_UP)
 
+sine_switch = Pin(11, Pin.IN, Pin.PULL_UP)
+square_switch = Pin(12, Pin.IN, Pin.PULL_UP)
+saw_switch = Pin(13, Pin.IN, Pin.PULL_UP)
+
 buttons = [
-    (button1, 262),
-    (button2, 294),
-    (button3, 330),
-    (button4, 349),
-    (button5, 392),
-    (button6, 440),
-    (button7, 494)
+    button1,
+    button2,
+    button3,
+    button4,
+    button5,
+    button6,
+    button7
 ]
 
-phase = 0
+frequencies = [
+    262,
+    294,
+    330,
+    349,
+    392,
+    440,
+    494
+]
+
+sine_table = []
+
+for i in range(256):
+    sine_table.append(
+        math.sin(2 * math.pi * i / 256)
+    )
+
+phases = [0] * 7
+
+steps = []
+
+for frequency in frequencies:
+    steps.append(
+        int(frequency * 65536 / sample_rate)
+    )
+
+samples = bytearray(chunk_size * 2)
 
 while True:
 
     volume = (potent.read_u16() * 15000) // 65535
 
-    frequency = 0
+    active = []
 
-    for button, note in buttons:
-        if button.value() == 0:
-            frequency = note
-            break
+    for i in range(7):
+        if buttons[i].value() == 0:
+            active.append(i)
 
-    samples = bytearray()
+    if sine_switch.value() == 1:
+        sound = 1
 
-    for i in range(400):
+    elif square_switch.value() == 1:
+        sound = 2
 
-        if frequency > 0:
-            value = int(volume * math.sin(phase))
+    elif saw_switch.value() == 1:
+        sound = 3
 
-            phase += 2 * math.pi * frequency / sample_rate
+    else:
+        sound = 1
 
-            if phase >= 2 * math.pi:
-                phase -= 2 * math.pi
+    for sample in range(chunk_size):
 
+        value = 0
+
+        for i in active:
+
+            position = phases[i] >> 8
+
+            if sound == 1:
+                wave = sine_table[position]
+
+            elif sound == 2:
+                if position < 128:
+                    wave = 1
+                else:
+                    wave = -1
+
+            else:
+                wave = (position / 128) - 1
+
+            value += wave
+
+            phases[i] = (phases[i] + steps[i]) & 65535
+
+        if len(active) > 0:
+            value = int(value * volume / len(active))
         else:
             value = 0
 
-        samples += struct.pack("<h", value)
+        struct.pack_into(
+            "<h",
+            samples,
+            sample * 2,
+            value
+        )
 
     audio.write(samples)
